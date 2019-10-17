22c0a095d0bf03be25edf5d6c978f0775777db63
DannyJPN <56021423+DannyJPN@users.noreply.github.com>
2019-10-17 21:21:37 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/registerprojectmobile/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.registerprojectmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

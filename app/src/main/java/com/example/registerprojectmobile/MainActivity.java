e81b80b51436a74ccc0e0b8d2e44caceb4cb93cd
DannyJPN <krupadan@email.cz>
2019-12-10 23:27:26 +0100

    Creating Main and Register activities. app/build/intermediates/manifest_merge_blame_file/debug/manifest-merger-blame-debug-report.txt app/build/intermediates/manifest_merge_blame_file/debugAndroidTest/manifest-merger-blame-debug-androidTest-report.txt

--- app/src/main/java/com/example/registerprojectmobile/MainActivity.java
+++ app/src/main/java/com/example/registerprojectmobile/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.registerprojectmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button regbutton =    (Button) findViewById(R.id.registerbutton);
+        Button addbutton =    (Button)findViewById(R.id.addtogroupbutton);
+        Button searchbutton = (Button)findViewById(R.id.searchchildbutton);
+
+        regbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reg = new Intent(getApplicationContext(),RegisterActivity.class);
+                reg.putExtra("OriginActivity","Main");
+                startActivity(reg);
+            }
+        });
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reg = new Intent(getApplicationContext(),RegisterToGroupActivity.class);
+                reg.putExtra("OriginActivity","Main");
+                reg.putExtra("DefaultChild" ,"None");
+
+                startActivity(reg);
+            }
+        });
+        regbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reg = new Intent(getApplicationContext(),ChildListActivity.class);
+                reg.putExtra("OriginActivity","Main");
+                startActivity(reg);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/registerprojectmobile/businesslogic/Child.java
+++ app/src/main/java/com/example/registerprojectmobile/businesslogic/Child.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class Child extends Person
     {
         public int ID ;public int  getID(){return ID;} public void setID(int value){ID = value;}
-        public Address HomeAddress ;public Address getHomeAddress(){return HomeAddress;} public void setHomeAddress(Address value){HomeAddress = value;}
+        private Address HomeAddress ;public Address getHomeAddress(){return HomeAddress;} public void setHomeAddress(Address value){HomeAddress = value;}
         public InsuranceCompany Insurance ;public InsuranceCompany getInsurance(){return Insurance;} public void setInsurance(InsuranceCompany value){Insurance = value;}
         public String HealthState ;public String getHealthState(){return HealthState;} public void setHealthState(String value){HealthState = value;}
         public String Comments ;public String getComments(){return Comments;} public void setComments(String value){Comments = value;}
@@ -13,5 +13,5 @@ public class Child extends Person
         public String SchoolName ;public String getSchoolName(){return SchoolName;} public void setSchoolName(String value){SchoolName = value;}
         public Parent Mother ;public Parent getMother(){return Mother;} public void setMother(Parent value){Mother = value;}
         public Parent Father ;public Parent getFather (){return Father ;} public void setFather (Parent value){Father  = value;}
-    ;}
+    }
 
